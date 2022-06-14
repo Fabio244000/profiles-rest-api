f8e5055498b33c3b57f9e345846ea0bf86985576
Fabio <fabio.nm.19@gmail.com>
2022-06-13 22:11:21 -0500

    - Added create, retrieve, update, partial_update and destroy method for ViewSets

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,12 @@
-from .views import HelloApiView
-from django.urls import path
-'''
-This is only for APIViews
-'''
+from .views import HelloApiView, HelloViewSets
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('hello-view-set', HelloViewSets, basename='test-view-set')
+
 urlpatterns = [
-    path('hello-view/', HelloApiView.as_view(), name='test-hello-view'),
+    path('hello-view/', HelloApiView.as_view(), name='test-hello-view'),  # urls maps for APIViews
+    path('', include(router.urls))  # urls map for ViewSets using routers
 ]
